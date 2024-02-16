0c813c8ee0b6df121ab447523bae0a10aae5e13d
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-02-16 11:08:32 +0530

    Add Salary

--- Lab-4/salary.sh
+++ Lab-4/salary.sh
@@ -7,13 +7,13 @@ if [ $salary -ge 10000 -a $salary -lt 20000 ]
     echo "DA:$da"
     hra=$(awk "BEGIN { printf(\"%.2f\", (($salary * 0.20)+ $da)) }") 
     echo "Gross:$hra"
-elif [ $result -ge 20000 -a $salary -lt 30000 ]
+elif [ $salary -ge 20000 -a $salary -lt 30000 ]
     then 
     da=$(awk "BEGIN { printf(\"%.2f\", (($salary * 0.90)+ $salary)) }") 
     echo "DA:$da"
     hra=$(awk "BEGIN { printf(\"%.2f\", (($salary * 0.25)+ $da)) }") 
     echo "Gross:$hra"
-elif [ $result -ge 30000 ]
+elif [ $salary -ge 30000 ]
     then 
     da=$(awk "BEGIN { printf(\"%.2f\", (($salary * 0.95)+ $salary)) }") 
     echo "DA:$da"
