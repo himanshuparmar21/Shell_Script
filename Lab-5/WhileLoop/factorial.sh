34190671efa9a6e01203024ec2488bfb71a515d5
himanshuparmar21 <himanshu.parmar21204@gmail.com>
2024-03-21 20:15:17 +0530

    Edit Some Program

--- Lab-5/WhileLoop/factorial.sh
+++ Lab-5/WhileLoop/factorial.sh
@@ -1,9 +1,13 @@
 echo "Enter Number"
 read n
+
 i=1
 fac=1
+
 while [ $i -le $n ]
 do
-    fac=`expr $i \* $fac`
-    i=`expr $i + 1`
+    fac=$(( $i * $fac ))
+    i=$(( $i + 1 ))
 done
+
+echo "Factorial of $n is: $fac"
--- Lab-6/oddEvenFor.sh
+++ Lab-6/oddEvenFor.sh
@@ -1,6 +1,6 @@
 for i in {1..100}
 do
-    ans=`expr $i \% 2`
+    ans=`expr $i % 2`
     if [ $ans -ne 0 ]
     then 
     echo "$i is Odd"
